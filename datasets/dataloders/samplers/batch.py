ed4906f3149e3a3337047154e2943adfb5d79a2c
wljywp9975 <39057136+robot-wljywp@users.noreply.github.com>
2026-06-20 20:39:33 +0800

    Add files via upload

---
+++ datasets/dataloders/samplers/batch.py
@@ -0,0 +1,30 @@
+import random
+import numpy as np
+from typing import List
+from datasets.GroundAerialDataset import GroundAerialDataset
+from datasets.dataloders.samplers.base import BaseSample
+
+class BatchSample(BaseSample):
+    """
+    # Batch sampling for dataset
+    """
+    def __init__(self, dataset:GroundAerialDataset, shuffle:bool, max_batches:int):
+        print("Sampling Mechanism: BatchSample")
+        self.dataset = dataset
+        self.max_batches = max_batches
+        self.k = 1
+        self.shuffle = shuffle
+
+    def get_k(self):
+        return self.k
+
+    def __call__(self, batch_size:int) -> List[List[int]]:
+        indices = self.dataset.get_indices()
+        indices = np.sort(indices)
+        if self.shuffle: random.shuffle(indices)
+        # remove tail
+        indices = indices[:indices.shape[0] - indices.shape[0] % batch_size]
+        # reshape to (batches, batch_size) & tolist
+        batch_idx = indices.reshape((-1, batch_size)).tolist()
+        return batch_idx
+
