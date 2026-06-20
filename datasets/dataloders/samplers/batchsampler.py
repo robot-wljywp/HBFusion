de15429afce1e45ab6b1806d945ad54cc0a5411a
wljywp9975 <39057136+robot-wljywp@users.noreply.github.com>
2026-06-20 20:38:39 +0800

    Enhance BatchSampler with sampling strategy selection
    Added support for selecting different sampling strategies and initialized GPU mode detection.

--- datasets/dataloders/samplers/batchsampler.py
+++ datasets/dataloders/samplers/batchsampler.py
@@ -1,5 +1,6 @@
 from typing import List
 from torch.utils.data import Sampler
+import torch.distributed as dist
 from datasets.GroundAerialDataset import GroundAerialDataset
 class BatchSampler(Sampler[List[int]]):
     """
@@ -8,6 +9,7 @@ class BatchSampler(Sampler[List[int]]):
     def __init__(
         self, 
         dataset:GroundAerialDataset, 
+        name:str,
         batch_size:int, 
         batch_size_limit:int,
         batch_expansion_rate:float, 
@@ -15,14 +17,36 @@ class BatchSampler(Sampler[List[int]]):
     ):
         # sample factory
         self.sample_fn = None
-        from datasets.dataloders.samplers.hetero import HeteroTripletSample
-        self.sample_fn = HeteroTripletSample(dataset=dataset, **kw)
+        if name == "BatchSample":
+            from datasets.dataloders.samplers.batch import BatchSample
+            self.sample_fn = BatchSample(dataset=dataset, **kw)
+        elif name == "HeteroTripletSample":
+            from datasets.dataloders.samplers.hetero import HeteroTripletSample
+            self.sample_fn = HeteroTripletSample(dataset=dataset, **kw)
+        else:
+            raise NotImplementedError("GroundAerialBatchSampler: %s sample_fn not implemented" % name)
+
+        # gpu mode
+        self.use_dist = False
+        if dist.is_initialized():
+            # multi-gpu
+            self.use_dist = True
+            if dist.get_rank() == 0: print("GroundAerialBatchSampler: multi-gpu mode")
+        else:
+            # single-gpu
+            print("GroundAerialBatchSampler: single-gpu mode")
+        
 
         self.batch_size = batch_size - batch_size%self.sample_fn.get_k()
         self.batch_size_limit = batch_size_limit
         self.batch_expansion_rate = batch_expansion_rate
+        if batch_expansion_rate is not None:
+            assert batch_expansion_rate > 1., "GroundAerialBatchSampler: batch_expansion_rate must be greater than 1"
+            assert batch_size <= batch_size_limit, "GroundAerialBatchSampler: batch_size_limit must be greater or equal to batch_size"
+
         self.batch_idx = []
         
+
 
     def __iter__(self):
         """
